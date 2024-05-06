7530993719b0127496c6ab0a56f7e37d884d8ef5
zhalgasbolatov <gfk575@gmail.com>
2024-05-06 17:49:28 +0500

    add some features

--- internal/broadcast.go
+++ internal/broadcast.go
@@ -7,26 +7,35 @@ func broadcast() {
 	for {
 		select {
 		case msg := <-join:
+
 			for _, client := range clients {
 				welcome := fmt.Sprintf("[%s][%s]:", msg.date, client.name)
+
 				if client.name != msg.name {
 					client.conn.Write([]byte(msg.text + "\n"))
 					client.conn.Write([]byte(welcome))
 				}
 			}
+
 		case msg := <-messages:
+
 			text := fmt.Sprintf("[%s][%s]:%s", msg.date, msg.name, msg.text)
+
 			for _, client := range clients {
 				metaDataClient := fmt.Sprintf("[%s][%s]:", msg.date, client.name)
+
 				if client.name != msg.name {
 					client.conn.Write([]byte("\r\n" + text + "\n"))
 					client.conn.Write([]byte(metaDataClient))
 				}
 			}
 			cahe = append(cahe, text+"\n")
+
 		case msg := <-leave:
+
 			for _, client := range clients {
 				metaDataClient := fmt.Sprintf("[%s][%s]:", msg.date, client.name)
+
 				if client.name != msg.name {
 					client.conn.Write([]byte("\r\n" + msg.text + "\n"))
 					client.conn.Write([]byte(metaDataClient))
